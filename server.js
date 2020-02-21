05be116e1097e46920216a467238436d9e968756
Askat B <Askat@MacBook-Pro-2.home>
2020-02-20 23:34:36 -0500

    refactor code

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ require("dotenv").config();
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
