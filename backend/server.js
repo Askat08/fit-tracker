018f0c8c45448eb7a817a52cc79a357968ef6978
Askat B <Askat@MacBook-Pro-2.home>
2020-02-20 23:18:03 -0500

    server file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
