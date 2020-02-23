a9726ec24b44454d94df42bd0456d21b6efd3f9d
Askat B <Askat@MacBook-Pro-2.home>
2020-02-22 19:08:28 -0500

    bug fixing

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 let Exercise = require("../models/exercise.model");
 
-router.get("/", (req, res) => {
+router.route("/").get((req, res) => {
   Exercise.find()
     .then(exercises => res.json("huuihuii", exercises))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.post("/add", (req, res) => {
+router.route("/add").post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = req.body.duration;
@@ -26,19 +26,19 @@ router.post("/add", (req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.get("/:id", (req, res) => {
+router.route("/:id").get((req, res) => {
   Exercise.findById(req.params.id)
     .then(exercise => res.json(exercise))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.delete("/:id", (req, res) => {
+router.route("/:id").delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
     .then(() => res.json("Exercise deleted."))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.post("/update/:id", (req, res) => {
+router.route("/update/:id").post((req, res) => {
   Exercise.findById(req.params.id)
     .then(exercise => {
       exercise.username = req.body.username;
