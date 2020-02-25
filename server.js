5608f8c1907a3f01a2515f8a09533b8688314f07
Askat B <Askat@MacBook-Pro-2.home>
2020-02-25 00:11:00 -0500

    urlencoded setting

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Cors setup
 // app.use((req, res, next) => {
